54cde6ef74a968cab21f8abad85dae256053bbb3
dangxiaobin123 <dangxiaobin123@163.com>
2020-02-25 12:08:31 +0800

    437. 路径总和 III

--- leetcode/0437. 路径总和 III/TEST.cpp
+++ leetcode/0437. 路径总和 III/TEST.cpp
@@ -2,30 +2,29 @@
 #include <catch.hpp>
 #include "solution.h"
 
-TEST_CASE("235. 二叉搜索树的最近公共祖先", "[lowestCommonAncestor]")
+TEST_CASE("437. 路径总和 III", "[pathSum]")
 {
-    SECTION("给定二叉树 [6,2,8,0,4,7,9,null,null,3,5]") {
-        TreeNode root(6);
-        TreeNode node1(2);
-        TreeNode node2(8);
-        TreeNode node3(0);
-        TreeNode node4(4);
-        TreeNode node5(7);
-        TreeNode node6(9);
-        TreeNode node7(3);
-        TreeNode node8(5);
+    SECTION("给定二叉树 [10,5,-3,3,2,null,11,3,-2,null,1]") {
+        TreeNode root(10);
+        TreeNode node1(5);
+        TreeNode node2(-3);
+        TreeNode node3(3);
+        TreeNode node4(2);
+        TreeNode node5(11);
+        TreeNode node6(3);
+        TreeNode node7(-2);
+        TreeNode node8(1);
 
         root.left = &node1;
         root.right = &node2;
         node1.left = &node3;
         node1.right = &node4;
-        node2.left = &node5;
-        node2.right = &node6;
-        node4.left = &node7;
-        node5.right = &node8;
+        node2.right = &node5;
+        node3.left = &node6;
+        node3.right = &node7;
+        node4.right = &node8;
 
         Solution s;
-        REQUIRE(s.lowestCommonAncestor(&root, &node1, &node2)->val == root.val);
-        REQUIRE(s.lowestCommonAncestor(&root, &node1, &node4)->val == node1.val);
+        REQUIRE(s.pathSum(&root, 8) == 3);
     }
 }
--- leetcode/0437. 路径总和 III/solution.h
+++ leetcode/0437. 路径总和 III/solution.h
@@ -12,6 +12,15 @@ struct TreeNode {
 class Solution {
 public:
     int pathSum(TreeNode* root, int sum) {
-        
+        if(!root) return 0;
+        return helper(root, sum)+pathSum(root->left, sum)+pathSum(root->right, sum);
+    }
+
+    int helper(TreeNode *root, int sum) {
+        if(!root) return 0;
+        int ans = root->val==sum?1:0;
+        ans+=helper(root->left, sum-root->val);
+        ans+=helper(root->right, sum-root->val);
+        return ans;
     }
 };
