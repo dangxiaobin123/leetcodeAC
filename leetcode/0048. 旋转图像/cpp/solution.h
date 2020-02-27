82b7eef9ee5a9de172eeb9af50a326fd146233a0
dangxiaobin123 <dangxiaobin123@163.com>
2020-02-27 10:30:07 +0800

    0048. 旋转图像 go solution

--- leetcode/0048. 旋转图像/cpp/solution.h
+++ leetcode/0048. 旋转图像/cpp/solution.h
@@ -4,8 +4,18 @@ using namespace std;
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        flip(matrix);
-        flip_vert(matrix);
+        int n = matrix.size();
+        for (int i = 0; i < n/2; i++)
+        {
+            for (int j = i; j<n-i-1; j++) {
+                swap(matrix[i][j], matrix[n-j-1][i]);
+                swap(matrix[n-j-1][i], matrix[n-i-1][n-j-1]);
+                swap(matrix[n-i-1][n-j-1], matrix[j][n-i-1]);
+            }
+        }
+        
+        // flip(matrix);
+        // flip_vert(matrix);
     }
 
     void flip_vert(vector<vector<int>>& matrix) {
